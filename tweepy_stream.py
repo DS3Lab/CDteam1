3e5bd2eecb939166208656e3c0f425e2793baa33
matijapiskorec <matija.piskorec@gmail.com>
2018-06-05 22:00:36 +0000

    Add tweepy filter keywords to config.ini

--- tweepy_stream.py
+++ tweepy_stream.py
@@ -6,11 +6,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-
-
 conf = utils.get_config()
-
-
 
 access_token = conf["Credentials"]["access_token"]
 access_token_secret = conf["Credentials"]["access_token_secret"]
@@ -18,20 +14,13 @@ consumer_key = conf["Credentials"]["consumer_key"]
 consumer_secret = conf["Credentials"]["consumer_secret"]
 raw_pass = conf["Credentials"]["raw_pass"]
 
-
-# print(consumer_key)
-
-
 url = "mongodb://CDteam1:" + urllib.parse.quote_plus(raw_pass) + "@spaceml4.ethz.ch:27017/CDteam1DB"
 client = MongoClient(url)
 db = client.CDteam1DB
 foo = db.foo
 
-
 class StdOutListener(StreamListener):
-    """
 
-    """
     def on_data(self, data):
         data = json.loads(data)
 
@@ -44,9 +33,8 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
 
-
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
-    stream.filter(track=["bitcoin","Ethereum","litecoin","cryptocurrency","blockchain"])
+    stream.filter(track=conf["Parameters"]["keywords"])
