1c7d4cc1882ca52e730304794ad1ad18146ad7a9
matijapiskorec <matija.piskorec@gmail.com>
2018-06-07 10:09:02 +0000

    Add multiple Twitter API credentials and a new MongoDB collection

--- tweepy_stream.py
+++ tweepy_stream.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("/home/ubuntu/anaconda2/envs/ds3/lib/python3.6/site-packages/")
 import utils
 import json
 from pymongo import MongoClient
@@ -5,18 +7,20 @@ import urllib
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from tweet_analysis import sentiment
+
 
 conf = utils.get_config()
 
 def credentials(i):
 
     if ( i == 1):
-
         access_token = conf["Credentials1"]["access_token"]
         access_token_secret = conf["Credentials1"]["access_token_secret"]
         consumer_key = conf["Credentials1"]["consumer_key"]
         consumer_secret = conf["Credentials1"]["consumer_secret"]
         raw_pass = conf["Credentials1"]["raw_pass"]
+
     elif (i == 2):
         access_token = conf["Credentials2"]["access_token"]
         access_token_secret = conf["Credentials2"]["access_token_secret"]
@@ -31,7 +35,17 @@ class StdOutListener(StreamListener):
     def on_data(self, data):
 
         data = json.loads(data)
-        foo.insert_one(data)
+        # print(data['text'])
+        #for key, value in data.items():
+         #     if key == 'text':
+          #          #print (value)
+           #         print(sentiment(value))
+        # print(sentiment(data["text"]))
+        data["sentiment"] = sentiment(data["text"])
+        data["timestamp_ms"] = int(data["timestamp_ms"])
+        data["timestamp_ms_int"] = int(data["timestamp_ms"])
+        # print(data)
+        tweets.insert_one(data)
         return True
 
     def on_error(self, status):
@@ -39,6 +53,7 @@ class StdOutListener(StreamListener):
 
 
 if __name__ == '__main__':
+
     i=1
     while True:
         access_token, access_token_secret, consumer_key, consumer_secret, raw_pass = credentials(i)
@@ -46,21 +61,20 @@ if __name__ == '__main__':
         url = "mongodb://CDteam1:" + urllib.parse.quote_plus(raw_pass) + "@spaceml4.ethz.ch:27017/CDteam1DB"
         client = MongoClient(url)
         db = client.CDteam1DB
-        foo = db.foo
+        tweets = db.tweets
         try:
             access_token, access_token_secret, consumer_key, consumer_secret, raw_pass = credentials(i)
             l = StdOutListener()
             auth = OAuthHandler(consumer_key, consumer_secret)
             auth.set_access_token(access_token, access_token_secret)
             stream = Stream(auth, l)
-            stream.filter(track=conf["Parameters"]["keywords"])
+            # print(conf["Parameters"]["keywords"])
+            stream.filter(languages=["en"], track=conf["Parameters"]["keywords"])
 
         except KeyboardInterrupt:
                 print("Shutting down!!")
                 raise
 
         except:
-            if i == 1:
-                i = 2
-            else:
-                i = 1
+            if i == 1: i = 2
+            else: i = 1
